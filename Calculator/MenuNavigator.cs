f5f901abaeb9470e8829f882077f846a7fdcb352
Daniel Johansson <dannesnake@gmail.com>
2024-05-29 10:53:46 +0200

    Add class MenuNavigator class containing method NavigateMenu. Not implemented and returns 0 to solve compiler error.

---
+++ Calculator/MenuNavigator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class MenuNavigator
+    {
+        public static int NavigateMenu(int firstRow, int lastRow, int cursorPosition)
+        {
+            return 0;
+        }
+    }
+}
