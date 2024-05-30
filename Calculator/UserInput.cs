bbdb3498819db2bec0ac71a1426995b1a6f495ff
Daniel Johansson <dannesnake@gmail.com>
2024-05-30 12:21:53 +0200

    Creating tests for InputHandler and implementing logic to pass tests Created interface IUserInput and UserInput to be able to mock user input values to the method ReceiveUserInput. ReceiveUserInput takes the interface as argument (dependecy injection). Created tests with mock and added logic to the method at the same time. Method passes all the tests. Don't know if I should add a message to user if unable to parse the string instead of just iterate to let the user enter a new string.

---
+++ Calculator/UserInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class UserInput : IUserInput
+    {
+        public string GetInput()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
